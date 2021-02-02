e561c9bac7eb0ba88e190ce0c5cebeb5f48f8011
Ardee Aram <ardee.aram@gmail.com>
2021-02-02 02:33:59 +0000

    Fix: netlify build

--- express/src/index.js
+++ express/src/index.js
@@ -22,6 +22,10 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.json({message: "Hello, world"});  
+});
+
 //OAuth Step 1
 app.post('/twitter/oauth/request_token', async (req, res) => {
   
